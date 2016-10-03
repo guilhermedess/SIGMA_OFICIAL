dbc8ff075f502eb8bd8b6e94c32cf1d57683aa0a
Guilherme de Souza Silveira <guilherme.souzaaa@live.com>
2016-10-03 16:10:19 -0300

    Campos ok

---
+++ USJT.Sigma.Repositorio/TopicoREP.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using USJT.Sigma.DataAccess;
+using USJT.Sigma.Model;
+
+namespace USJT.Sigma.Repositorio
+{
+    public class TopicoREP
+    {
+        public int AdicionaTopico(TB_ALUNO aluno, string nomeTopico)
+        {
+            using (var conexao = new SIGMAEntities())
+            {
+                //var alunoNovo = conexao.TB_ALUNO.Single(x => x.ID_ALUNO == aluno.IdAluno);
+
+                //verificar se o topico já existe, adiciono na tabela Topico uma linha 
+
+                var idTopicoProcurado = ExisteTopico(aluno.ID_ALUNO, nomeTopico);
+                if (idTopicoProcurado == 0)
+                {
+                    var novoTopico = new TB_TOPICO();
+
+                    novoTopico.ID_ALUNO = aluno.ID_ALUNO;
+                    novoTopico.NOM_TOPICO = nomeTopico;
+
+                    conexao.TB_TOPICO.Add(novoTopico);
+                    conexao.SaveChanges();
+
+                    return novoTopico.ID_TOPICO;
+                }
+                else
+                {
+                    return idTopicoProcurado;
+                }
+            }
+        }
+
+        public int ExisteTopico(int idAluno, string nomeTopico)
+        {
+            using (var conexao = new SIGMAEntities())
+            {
+                //var retorno = (from C in conexao.TB_TOPICO
+                //               where C.ID_ALUNO == idAluno && C.NOM_TOPICO == nomeTopico
+                //               select new Topico
+                //               {
+                //                   IdAluno = idAluno,
+                //                   Nome = nomeTopico,
+                //               }).ToList();
+
+                var retorno = from A in conexao.TB_TOPICO
+                          where A.ID_ALUNO == idAluno && A.NOM_TOPICO == nomeTopico
+                          select A;
+
+                //Topico topicoRecuperado;
+                int idTopicoRecuperado = 0;
+
+                foreach (var aux in retorno)
+                    idTopicoRecuperado = (int) aux.ID_TOPICO;
+
+                return idTopicoRecuperado;
+            }
+        }
+
+        
+    }
+}
---
+++ USJT.Sigma.UI.WEB/Controllers/SubAmostragemController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using USJT.Sigma.Model;
+using USJT.Sigma.Repositorio;
+
+namespace USJT.Sigma.UI.WEB.Controllers
+{
+    public class SubAmostragemController : Controller
+    {
+        SubTopicoREP subTopicoREP = new SubTopicoREP();
+        AlunoREP alunoREP = new AlunoREP();
+
+        public ActionResult ImpAmostragem()
+        {
+            var dadosAlunoLogado = Session["dadosAlunoLogado"];
+
+            return View(dadosAlunoLogado);
+        }
+
+        [HttpPost]
+        public ActionResult ImpAmostragem(Aluno aluno, Atividade atividade/*Aluno aluno, SubTopico subTopico, bool feito*/)
+        {
+            //var nomeTopico = "Amostragem";
+            //var nomeSubTopico = "Importância da Amostragem";
+
+            //aluno = (Aluno) Session["dadosAlunoLogado"];
+
+            ////var feito = true;
+
+            //if (atividade.Resposta == true)
+            //{
+            //    //consulta atividade pra ver se ja existe
+
+            //    int idSubTopicoAdicionado = subTopicoREP.AdicionaSubTopico(aluno, nomeTopico, nomeSubTopico/*, feito*/);
+
+            //    var nomeAtividade = "AtvImpAmostragem";
+
+            //    //parei aqui
+
+            //    AtividadeREP atividadeREP = new AtividadeREP();
+            //    atividadeREP.AdicionaAtividade(aluno.IdAluno, idSubTopicoAdicionado, nomeAtividade);
+            //}
+
+            //return View();
+
+            AtividadeREP atividadeREP = new AtividadeREP();
+            var nomeTopico = "Amostragem";
+            var nomeSubTopico = "Importância da Amostragem";
+
+            aluno = (Aluno)Session["dadosAlunoLogado"];
+
+            if (atividade.Resposta)
+            {
+                var nomeAtividade = "AtvImpAmostragem";
+
+                //consulta atividade pra ver se ja existe
+                if (atividadeREP.ExisteAtividade(aluno.IdAluno, nomeAtividade))
+                {
+                    return View();
+                }
+                else
+                {
+                    int idSubTopicoAdicionado = subTopicoREP.AdicionaSubTopico(aluno, nomeTopico, nomeSubTopico);
+
+                    atividadeREP.AdicionaAtividade(aluno.IdAluno, idSubTopicoAdicionado, nomeAtividade);
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Conceitos()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Conceitos(SubTopico subTopico, bool feito)
+        {
+            feito = true;
+
+            //subTopicoREP.checkSubTopico(feito);
+
+            return View();
+        }
+
+        public ActionResult AleatoriaSimples()
+        {
+            return View();
+        }
+
+        public ActionResult AleatoriaEstratificada()
+        {
+            return View();
+        }
+
+        public ActionResult Conglomerado()
+        {
+            return View();
+        }
+
+        public ActionResult Sistematica()
+        {
+            return View();
+        }
+
+    }
+}
